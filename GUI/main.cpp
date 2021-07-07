9fe06ee8c542f18710880adc654dfc0c1ac08652
Tonix22 <emiliotonix@gmail.com>
2021-07-07 15:15:11 -0500

    GUI Interface working

---
+++ GUI/main.cpp
@@ -0,0 +1,16 @@
+#include "pnlAPI.h"
+//#include "training.h"
+
+wxIMPLEMENT_APP(MyApp);
+/*
+int main(int argc, char const *argv[])
+{
+    //pnlAPI* api = new pnlAPI();
+    //api->Initialize(lmsControl);
+    //api->Connect(wxEVT_CLOSE_WINDOW, wxCloseEventHandler(LMS7SuiteAppFrame::OnAPIClose), NULL, this);
+    //api->Show();
+    MyApp app;
+    app.OnInit();
+
+    return 0;
+}*/
